f3335a9dd897aa12c632a9c26c8695d72073c1e1
Ante <ante.petrovic1002@gmail.com>
2024-12-04 00:46:37 +0100

    add navbar first part

--- client/src/Pages/HomePage/Home.js
+++ client/src/Pages/HomePage/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Header from '../../Components/HeaderComponent/Header'
+import Navbar from '../../Components/NavbarComponent/Navbar'
 
 const Home = () => {
   return (
     <main>
         <Header/>
+        <Navbar/>
         Home
     </main>
   )
