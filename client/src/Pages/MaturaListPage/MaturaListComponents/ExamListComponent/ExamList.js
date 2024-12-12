c96bd69cfe0b5a134795a26ab955add80ae9b43d
Ante <ante.petrovic1002@gmail.com>
2024-12-12 19:08:07 +0100

    add some boilerplate code

--- client/src/Pages/MaturaListPage/MaturaListComponents/ExamListComponent/ExamList.js
+++ client/src/Pages/MaturaListPage/MaturaListComponents/ExamListComponent/ExamList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './examList.css'
 
 const ExamList = ({ data }) => {
   if (!data || data.length === 0) {
@@ -7,12 +8,15 @@ const ExamList = ({ data }) => {
   }
 
   return (
-    <div>
-      <h1>Popis ispita</h1>
+    <div className="exam-list-main-wrap">
+      <h1 className="exam-list-title">Popis online državnih matura</h1>
+      <h2 className="exam-list-subtitle">
+        Odaberi jednu od ponuđenih online matura i započmi s riješavanjem!!
+      </h2>
       <ul>
         {data.map((exam) => (
           <li key={exam.id}>
-            <Link to={`/exam/${exam.id}`}>
+            <Link className="exam-list-link" to={`/exam/${exam.id}`}>
               {`${exam.predmet} ${exam.year}, ${exam.term}`}
             </Link>
           </li>
--- server/routes/registrationRoutes.js
+++ server/routes/registrationRoutes.js
@@ -10,7 +10,8 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
-    }
+    },
+    tls: {rejectUnauthorized : false}
 });
 
 //Ruta za registraciju korisnika
